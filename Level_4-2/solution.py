eef9a3ccc11b713b3e52326693d7dbce919f3a83
appinha <amanda_pinha@hotmail.com>
2022-10-29 20:56:36 -0300

    Level 4.2: not finished

---
+++ Level_4-2/solution.py
@@ -0,0 +1,6 @@
+def solution(times, time_limit):
+  """calculate the most bunnies you can pick
+up and which bunnies they are
+return the set of bunnies with the
+lowest worker IDs (as indexes) in sorted order"""
+  ...
